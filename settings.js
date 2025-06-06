d46dba4cea4ae3e22f1959f3e64c71297a7705b8
rema-17 <frevelateur@gmail.com>
2025-06-07 00:10:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "revelateur",
   botOwner: 'revelateur', // Your name
-  ownerNumber: 237621703294', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: 23721703294', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
